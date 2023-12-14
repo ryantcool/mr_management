81f4da774bad4431532238373db8261d17dfbdee
Ryan Cool <ryan.cool@tuta.io>
2023-12-14 17:00:53 -0500

    first commit for managing some scripts used on the MRRC

---
+++ batch_run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# This is a batch file that is called by prompt_run.sh
+#
+# This file utilizes the mr_manager script
+# and executes it on the name's of the PI's whose scan's you're
+# looking to manage, as well as the names of the scanners used
+# as defined in the arrays labeled pis and scanners
+
+# Change this to match th pi's you are working with
+pis=('esterlis' 'cosgrove' 'davis' 'hillmer' 'morris')
+# Change this to the scanners used
+scanners=('prismaa' 'prismab' 'prismac')
+
+for scanner in "${scanners[@]}";
+do
+    for pi in "${pis[@]}";
+    do
+        # Change this to location of where the mr_manager script is on your system
+        /home/"$USER"/codes/pseudo_cron_files/mr_manager backup "$scanner" "$pi"
+    done
+done
---
+++ prompt_run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# This is a simple script that can
+# generate a prompt when ssh-ing to a server
+#
+# Add:
+# 	RequestTTY yes
+#	RemoteCommand /path/to/file/on/remote/server/prompt_run.sh; exec tcsh  -l
+#
+# To the ~/.ssh/config entry of the host you're connecting to
+#
+# RequestTTY yes will always request a TTY when standard input is a TTY
+# exec tcsh -l can be changed to work for whichever shell you're using on the server
+
+echo "Hello $USER! Would you like to check for mri's? (y/n)"
+read ANSWER
+if [[ $ANSWER == y || $ANSWER == Y || $ANSWER == YES || $ANSWER == Yes || $ANSWER == yes ]]; then
+	# Change this to the file path of where the script is on the remote server
+	/bin/bash -c /home/"$USER"/codes/pseudo_cron_files/batch_run.sh
+else
+	echo "Not checking"
+fi
